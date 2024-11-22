6404d2e01aa7cdecdf503e5163ed71c6f0e0b2f0
Rahul Jangir <rahuljangir4368@gmail.com>
2024-11-22 08:58:41 +0530

    asyc and syc

--- node js/file.js
+++ node js/file.js
@@ -1,17 +1,20 @@
 import fs from 'fs';
+import os from 'os';
+
+console.log(os.cpus().length);
 
 // fs.writeFileSync("contact.txt","Hey, Rahul jangir here")
-fs.writeFile("contact.txt","Hey, Rahul jangir here not sync",err =>{})
+// fs.writeFile("contact.txt","Hey, Rahul jangir here not sync",err =>{})
 
 
-const result = fs.readFileSync("./contact.txt","utf-8");
-console.log(result);
+// const result = fs.readFileSync("./contact.txt","utf-8");
+// console.log(result);
 
-fs.readFile("./contact.txt",(err,data)=>{
-    if(err)
-         console.log(err);
-    else{
-        console.log(data.toString());
-    }
-})
+// fs.readFile("./contact.txt",(err,data)=>{
+//     if(err)
+//          console.log(err);
+//     else{
+//         console.log(data.toString());
+//     }
+// })
 
