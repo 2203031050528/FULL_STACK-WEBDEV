c6ee784446133a1095ed66f155f0224810ccfd01
Rahul Jangir <rahuljangir4368@gmail.com>
2024-11-22 08:34:32 +0530

    new file add

---
+++ node js/file.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+// fs.writeFileSync("contact.txt","Hey, Rahul jangir here")
+fs.writeFile("contact.txt","Hey, Rahul jangir here not sync",err =>{})
+
+
+const result = fs.readFileSync("./contact.txt","utf-8");
+console.log(result);
+
+fs.readFile("./contact.txt",(err,data)=>{
+    if(err)
+         console.log(err);
+    else{
+        console.log(data.toString());
+    }
+})
+
---
+++ node js/node.js
@@ -0,0 +1,6 @@
+// main.js
+
+import { add, subtract } from './math.js';
+
+console.log(`The sum of 2 and 3 is: ${add(2, 3)}`); 
+console.log(`The difference of 5 and 2 is: ${subtract(5, 2)}`);
